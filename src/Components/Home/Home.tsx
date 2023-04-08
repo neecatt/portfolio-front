76fbf8fc7778b3e0ae71ffd32ea62577e5227bc5
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-08 14:19:08 +0400

    Initialized home page

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,21 +1,18 @@
-import { Box, Img, Text } from '@chakra-ui/react'
-import React from 'react'
-import './Home.css'
+import { Box, Img, Text } from "@chakra-ui/react";
+import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
     <>
-    <Box display={'flex'} justifyContent={'center'} alignContent='center' className=' justify-center align-middle'>
-      <Text className='flex max-w-lg justify-center item-center text-5xl'>I'm<Text as='span' className='font-bold text-blue-300'>Nijat Abdullazada</Text>, Web Developer.
-      </Text>
-      <Img></Img>
-    </Box>
-    
-    
-    
-    
+      <Box
+        display={"flex"}
+        justifyContent={"center"}
+        alignContent="center"
+        className=" justify-center align-middle"
+      ></Box>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
