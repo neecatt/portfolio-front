b66bcefbfb497b1335afb815b8a83e3dac76e96a
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-08 14:18:42 +0400

    syntax change

--- src/Components/Admin/Admin.tsx
+++ src/Components/Admin/Admin.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./Admin.css";
-import profile from '../../img/profile.png'
+import profile from "../../img/profile.png";
 
 const Admin = () => {
   const [textBoxValue, setTextBoxValue] = useState("");
@@ -27,24 +27,18 @@ const Admin = () => {
 
   return (
     <div className="">
-        <h1 className=" bg-white text-4xl font-bold text-center text-black object-top">
-          Admin Panel
-        </h1>
-      
+      <h1 className=" bg-white text-4xl font-bold text-center text-black object-top">
+        Admin Panel
+      </h1>
+
       {/* side bar in white color using tailwind */}
       <div className="flex-row">
         {/* add two seperate white boxes using tailwind in to the center */}
-        
-              
       </div>
       <div className="flex flex-row">
         <div className="rounded-b-lg flex flex-col w-1/5 h-screen bg-white">
           <div className="flex flex-col items-center justify-center w-full h-1/6">
-            <img
-              className="w-20 h-20 rounded-full"
-              src=""
-              alt="profile"
-            />
+            <img className="w-20 h-20 rounded-full" src="" alt="profile" />
 
             <h1 className="text-xl font-bold text-center text-black">Admin</h1>
           </div>
@@ -55,22 +49,22 @@ const Admin = () => {
               </button>
             </div>
             <div className="flex flex-col items-center justify-center w-full h-1/6">
-            <button className="rounded-xl border-2 text-xl  w-[8rem] text-center text-black font-[Courier New] hover:bg-green-400 hover:text-white">
+              <button className="rounded-xl border-2 text-xl  w-[8rem] text-center text-black font-[Courier New] hover:bg-green-400 hover:text-white">
                 Education
               </button>
             </div>
             <div className="flex flex-col items-center justify-center w-full h-1/6">
-            <button className="rounded-xl border-2 text-xl  w-[8rem]  text-center text-black font-[Courier New] hover:bg-green-400 hover:text-white">
+              <button className="rounded-xl border-2 text-xl  w-[8rem]  text-center text-black font-[Courier New] hover:bg-green-400 hover:text-white">
                 Skills
               </button>
             </div>
             <div className="flex flex-col items-center justify-center w-full h-1/6">
-            <button className="rounded-xl border-2 text-xl  w-[8rem] text-center text-black font-[Courier New] hover:bg-green-400 hover:text-white">
+              <button className="rounded-xl border-2 text-xl  w-[8rem] text-center text-black font-[Courier New] hover:bg-green-400 hover:text-white">
                 Projects
               </button>
             </div>
             <div className="flex flex-col items-center justify-center w-full h-1/6">
-            <button className="rounded-xl border-2 text-xl  w-[8rem]  text-center text-black font-[Courier New] hover:bg-green-400 hover:text-white">
+              <button className="rounded-xl border-2 text-xl  w-[8rem]  text-center text-black font-[Courier New] hover:bg-green-400 hover:text-white">
                 Dashboard
               </button>
             </div>
@@ -79,11 +73,11 @@ const Admin = () => {
         <div className="flex  flex-col gap-4  w-4/5 m-2 h-screen ">
           <div className="rounded-xl   bg-white flex flex-col items-center justify-center w-full h-2/6">
             yuxari
-            </div>
-            <div className="flex rounded-xl flex-col bg-white  items-center justify-center w-full h-4/6">
-              asagi
-              </div>
           </div>
+          <div className="flex rounded-xl flex-col bg-white  items-center justify-center w-full h-4/6">
+            asagi
+          </div>
+        </div>
       </div>
       {/* <input className='rounded-md flex bg-white cursor-text text-black' type="text" value={textBoxValue} onChange={handleTextBoxChange} />
        <button className='border-radius-[1em] bg-slate-600' onClick={handleSubmit}>Submit</button> */}
