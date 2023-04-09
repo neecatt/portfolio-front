bfac576a9816a458e76cc19bccc4d08311714081
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-09 15:27:20 +0400

    added transition functionality

--- src/Components/StartScreen.tsx
+++ src/Components/StartScreen.tsx
@@ -2,18 +2,30 @@ import React, { useEffect, useState } from "react";
 import "animate.css";
 import { Box, Heading, Text } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 function StartScreen() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      navigate("/home");
+    }, 3700);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
   return (
     <Box
       height="100vh"
       display="flex"
       justifyContent="center"
       alignItems="center"
-      flexDirection="row"
+      flexDirection="column"
       backgroundColor={"black"}
       gap={4}
-      className="animate__animated animate__fadeOut animate__delay-3s"
+      className="animate__animated animate__fadeOut animate__delay-3s "
     >
       <Text
         fontWeight="light"
