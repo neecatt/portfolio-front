f516e67d72197f9d5f0add2337291ea1a74e5434
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-09 15:22:08 +0400

    added startscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import Admin from './Components/Admin/Admin'
-import './App.css'
-import Navbar from './Components/Navbar/Navbar'
-import { BrowserRouter , Route, Routes } from 'react-router-dom'
-import Home from './Components/Home/Home'
-
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import Admin from "./pages/Admin/Admin";
+// import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import StartScreen from "./components/StartScreen";
+import "animate.css/animate.min.css";
 
 function App() {
+  const [showStartScreen, setShowStartScreen] = useState(true);
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowStartScreen(false);
+    }, 3700);
 
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
   return (
-    
     <BrowserRouter>
-    <Navbar/>
-    <div className="App">
-      <Routes>
-        <Route path='/home' element={<Home/>}></Route>
-        <Route path='/admin' element={<Admin/>}></Route>
-      </Routes>
-    </div>
+      {/* {showStartScreen && <StartScreen />} */}
+      <div className="App">
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/admin" element={<Admin />}></Route>
+        </Routes>
+      </div>
     </BrowserRouter>
-   
-  )
+  );
 }
 
-export default App
+export default App;
