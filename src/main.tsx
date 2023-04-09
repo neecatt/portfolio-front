1f21a9f7542ab01f93683aba190498254a577847
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-09 15:24:34 +0400

    modified default chakra ui bg color

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import 'tailwindcss/tailwind.css'
-import { ChakraBaseProvider } from '@chakra-ui/react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "tailwindcss/tailwind.css";
+import { ChakraBaseProvider } from "@chakra-ui/react";
 import { extendTheme, ChakraProvider } from "@chakra-ui/react";
 
 const theme = extendTheme({
   styles: {
     global: {
       body: {
-        bg: "gray.100",
+        bg: "black",
+        fontFamily: "YD Gothic 100",
+        color: "white",
       },
     },
   },
 });
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ChakraBaseProvider>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <ChakraBaseProvider theme={theme}>
     <App />
-    </ChakraBaseProvider>
-  </React.StrictMode>,
-)
+  </ChakraBaseProvider>
+);
