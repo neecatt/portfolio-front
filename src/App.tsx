823dbb3e7d4ea50f7e66d85ccceeb065eec3da36
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-13 14:03:50 +0400

    configured startscreen animation delay

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
-import Admin from "./pages/Admin/Admin";
+import Admin from "./components/Admin";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import "animate.css/animate.min.css";
+import StartScreen from "./components/StartScreen";
 
 function App() {
   const [showStartScreen, setShowStartScreen] = useState(true);
+  const [showHome, setShowHome] = useState(false);
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       setShowStartScreen(false);
-    }, 3700);
+      setShowHome(true);
+    }, 2500);
 
     return () => {
       clearTimeout(timeoutId);
@@ -18,10 +21,10 @@ function App() {
   }, []);
   return (
     <BrowserRouter>
-      {/* {showStartScreen && <StartScreen />} */}
+      {showStartScreen && <StartScreen />}
       <div className="App">
         <Routes>
-          <Route path="/home" element={<Home />}></Route>
+          {showHome && <Route path="/" element={<Home />}></Route>}
           <Route path="/admin" element={<Admin />}></Route>
         </Routes>
       </div>
