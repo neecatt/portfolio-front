aad9d7bb29f9b729edede437f0994c4b4124ea96
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-13 14:05:39 +0400

    added background video

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex, Img, Text } from "@chakra-ui/react";
 import { motion } from "framer-motion";
+import videoBg from "../../assets/videoBg.mp4";
 
 const Home = () => {
   const typingContainer = {
@@ -7,7 +8,7 @@ const Home = () => {
     show: {
       opacity: 1,
       transition: {
-        staggerChildren: 0.12,
+        staggerChildren: 0.08,
       },
     },
   };
@@ -24,38 +25,57 @@ const Home = () => {
   };
 
   return (
-    <Box
-      display="flex"
-      border={"1px solid white"}
-      width="100wv"
-      height={"100vh"}
-      margin="0"
-      padding="0"
-      boxSizing="border-box"
-    >
-      <Box px={4} py={6}>
-        <Flex justifyContent={"center"} flexDirection="column">
-          <Box fontSize={"5xl"} display={"inline-block"}>
-            <motion.div
-              variants={typingContainer}
-              initial="hidden"
-              animate="show"
+    <Box border={"1px solid red"}>
+      <Box
+        display="flex"
+        width="100%"
+        height="100%"
+        className="animate__animated animate__fadeIn"
+      >
+        <Box
+          flex={1}
+          position="fixed"
+          width="100vw"
+          height="100vh"
+          overflow="hidden"
+          zIndex="-999"
+        >
+          <video
+            autoPlay
+            loop
+            muted
+            src={videoBg}
+            className="w-screen h-screen object-cover opacity-50"
+          />
+        </Box>
+        <Box px={4} py={6}>
+          <Flex justifyContent={"center"} flexDirection="column">
+            <Box fontSize={"5xl"} display={"inline-block"} fontWeight="light">
+              <motion.div
+                variants={typingContainer}
+                initial="hidden"
+                animate="show"
+              >
+                {Array.from("Nijat Abdullazada").map((word, i) => (
+                  <motion.span key={i} variants={typingText}>
+                    {word}
+                  </motion.span>
+                ))}
+              </motion.div>
+            </Box>
+            <Text
+              className="animate__animated animate__fadeIn animate__delay-1s"
+              fontSize={"3xl"}
+              display={"inline-block"}
+              fontWeight="lighter"
+              sx={{
+                "--animate-delay": "1.4s",
+              }}
             >
-              {Array.from("Nijat Abdullazada").map((word, i) => (
-                <motion.span key={i} variants={typingText}>
-                  {word}
-                </motion.span>
-              ))}
-            </motion.div>
-          </Box>
-          <Text
-            className="animate__animated animate__fadeIn animate__delay-2s"
-            fontSize={"3xl"}
-            display={"inline-block"}
-          >
-            Web Developer
-          </Text>
-        </Flex>
+              Web Developer
+            </Text>
+          </Flex>
+        </Box>
       </Box>
     </Box>
   );
