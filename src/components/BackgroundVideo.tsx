dcf691fa71d39ca6d90417407257402251f23abc
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-16 21:39:25 +0400

    added bgvideo component

---
+++ src/components/BackgroundVideo.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import videoBg from "../assets/videoBg.mp4";
+
+type Props = {};
+
+const BackgroundVideo = (props: Props) => {
+  return (
+    <video
+      autoPlay
+      muted
+      loop
+      className="absolute top-0 left-0 w-full h-full object-cover opacity-[0.22]"
+      playsInline
+    >
+      <source src={videoBg} type="video/mp4" />
+    </video>
+  );
+};
+
+export default BackgroundVideo;
