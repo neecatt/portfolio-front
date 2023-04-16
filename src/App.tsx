fc3858afc830f70b7d5147958ed777d6177c9e2b
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-16 21:40:03 +0400

    added animation func to wholeapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import Admin from "./components/Admin";
+import Admin from "./pages/Admin/Admin";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import "animate.css/animate.min.css";
 import StartScreen from "./components/StartScreen";
+import Frame from "./components/Frame";
+import BackgroundVideo from "./components/BackgroundVideo";
 
 function App() {
   const [showStartScreen, setShowStartScreen] = useState(true);
@@ -13,7 +15,7 @@ function App() {
     const timeoutId = setTimeout(() => {
       setShowStartScreen(false);
       setShowHome(true);
-    }, 2500);
+    }, 2600);
 
     return () => {
       clearTimeout(timeoutId);
@@ -21,13 +23,18 @@ function App() {
   }, []);
   return (
     <BrowserRouter>
+      <Frame>
+        <BackgroundVideo />
+
+        <div className="App">
+          <Routes>
+            {showHome && <Route path="/" element={<Home />}></Route>}
+            <Route path="/admin" element={<Admin />}></Route>
+          </Routes>
+        </div>
+      </Frame>
+
       {showStartScreen && <StartScreen />}
-      <div className="App">
-        <Routes>
-          {showHome && <Route path="/" element={<Home />}></Route>}
-          <Route path="/admin" element={<Admin />}></Route>
-        </Routes>
-      </div>
     </BrowserRouter>
   );
 }
