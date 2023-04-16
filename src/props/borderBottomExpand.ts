d81dbc5f3accc9a73e3133eeab977540f47f7b28
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-16 21:38:57 +0400

    added bottomborderexpand prop

---
+++ src/props/borderBottomExpand.ts
@@ -0,0 +1,13 @@
+const borderBottomExpand = {
+  bgImage: "linear-gradient(#ffffff, #ffffff)",
+  bgSize: "0% 1px",
+  bgRepeat: "no-repeat",
+  bgPos: "0 calc(100% - 0px)",
+  _hover: {
+    bgSize: "100% 1px",
+  },
+  transition: "background-size 0.3s",
+  transformOrigin: "0% 50%",
+};
+
+export default borderBottomExpand;
