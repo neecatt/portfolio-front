4852cad1fdf6b1ff537736d55f5c42a6e2ed47a9
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-17 15:30:39 +0400

    deleted unused logic in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Button, Flex, Text } from "@chakra-ui/react";
 import borderBottomExpand from "../props/borderBottomExpand";
+import borderBottomExpandResume from "../props/borderBottomExpandResume";
 
 const Navbar: React.FC = () => {
   const [hideNavbar, setHideNavbar] = useState<boolean>(false);
   const [activeButton, setActiveButton] = useState<string>("");
-
-  useEffect(() => {
-    let prevScrollPos = window.pageYOffset;
-
-    const handleScroll = () => {
-      const currentScrollPos = window.pageYOffset;
-      const shouldHideNavbar = prevScrollPos < currentScrollPos;
-
-      setHideNavbar(shouldHideNavbar);
-      prevScrollPos = currentScrollPos;
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   return (
     <Flex
@@ -38,12 +21,14 @@ const Navbar: React.FC = () => {
       boxShadow={hideNavbar ? "sm" : "none"}
       transition="all 0.2s ease-in-out"
       zIndex={99}
+      className="animate__animated animate__fadeIn animate__delay-2s"
     >
       <Button
         fontWeight={"light"}
         bg={"transparent"}
         {...borderBottomExpand}
         borderBottom={activeButton === "Projects" ? "1px solid  white" : "none"}
+        _active={{ bg: "transparent" }}
         onClick={() => setActiveButton("Projects")}
       >
         Projects
@@ -55,6 +40,7 @@ const Navbar: React.FC = () => {
         borderBottom={
           activeButton === "Experience" ? "1px solid  white" : "none"
         }
+        _active={{ bg: "transparent" }}
         onClick={() => setActiveButton("Experience")}
       >
         Experience
@@ -72,9 +58,11 @@ const Navbar: React.FC = () => {
       <Button
         fontWeight={"light"}
         bg={"transparent"}
-        {...borderBottomExpand}
-        borderBottom={activeButton === "Resume" ? "1px solid white" : "none"}
+        {...borderBottomExpandResume}
+        _active={{ bg: "transparent" }}
         onClick={() => setActiveButton("Resume")}
+        //give color lineargradient  #AE67FA #F49867 both 100%
+        bgGradient="linear(to-r, #AE67FA, #F49867)"
       >
         Resume
       </Button>
