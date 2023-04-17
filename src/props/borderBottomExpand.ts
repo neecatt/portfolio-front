ee4f1dc42fea64f7e092042e42f5c73686fde029
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-17 15:31:44 +0400

    changed color of borderBottom

--- src/props/borderBottomExpand.ts
+++ src/props/borderBottomExpand.ts
@@ -1,9 +1,10 @@
 const borderBottomExpand = {
-  bgImage: "linear-gradient(#ffffff, #ffffff)",
+  bgImage: "linear-gradient(white, white)",
   bgSize: "0% 1px",
   bgRepeat: "no-repeat",
   bgPos: "0 calc(100% - 0px)",
   _hover: {
+    color: "white",
     bgSize: "100% 1px",
   },
   transition: "background-size 0.3s",
