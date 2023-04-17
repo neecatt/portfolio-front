b39ff7f28778f7f2939da720fa8dda3073497e32
HorrorOTB <nicatabdullazade135@gmail.com>
2023-04-17 21:21:05 +0400

    added about section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,8 @@
-import { Box, Flex, Img, Text } from "@chakra-ui/react";
+import { Box, Flex, Img, Text, Wrap } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import Navbar from "../../components/Navbar";
 import Socials from "../../components/Socials";
+import borderBottomExpand from "../../props/borderBottomExpand";
 
 const Home = () => {
   const typingContainer = {
@@ -29,54 +30,99 @@ const Home = () => {
     <>
       <Navbar />
       <Flex
-        border={"1px solid gray"}
         height="100vh"
         overflow="hidden"
         overscrollBehavior={"none"}
-        justifyContent={"flex-start"}
-        alignItems={"center"}
         className="animate__animated animate__fadeIn"
+        zIndex={-999}
+        px={4}
       >
         <Flex
-          className="animate__animated animate__fadeIn"
-          overflowY={"scroll"}
-          height={"100vh"}
-          ml={"1em"}
+          justifyContent={"start"}
+          flexDirection="column"
+          py={"10rem"}
+          // border="1px solid blue"
         >
-          <Flex justifyContent={"center"} flexDirection="column">
-            <Box
-              fontSize={{ sm: "5xl", md: "7xl", base: "3xl" }}
-              display={"inline-block"}
-              fontWeight="light"
+          <Box
+            fontSize={{ sm: "5xl", md: "7xl", base: "3xl" }}
+            display={"inline-block"}
+            fontWeight="light"
+          >
+            <motion.div
+              variants={typingContainer}
+              initial="hidden"
+              animate="show"
             >
-              <motion.div
-                variants={typingContainer}
-                initial="hidden"
-                animate="show"
-              >
-                {Array.from("Nijat Abdullazada").map((word, i) => (
-                  <motion.span key={i} variants={typingText}>
-                    {word}
-                  </motion.span>
-                ))}
-              </motion.div>
-            </Box>
-            <Box>
-              <Text
-                className="animate__animated animate__fadeIn animate__delay-1s"
-                fontSize={"3xl"}
-                display={"inline-block"}
-                fontWeight="lighter"
-                sx={{
-                  "--animate-delay": "1.4s",
-                }}
-              >
-                Web Developer
-              </Text>
-            </Box>
-          </Flex>
-          <Socials />
+              {Array.from("Nijat Abdullazada").map((word, i) => (
+                <motion.span key={i} variants={typingText}>
+                  {word}
+                </motion.span>
+              ))}
+            </motion.div>
+          </Box>
+          <Box>
+            <Text
+              className="animate__animated animate__fadeIn animate__delay-1s"
+              fontSize={"3xl"}
+              display={"inline-block"}
+              fontWeight="lighter"
+              sx={{
+                "--animate-delay": "1.4s",
+              }}
+            >
+              Web Developer
+            </Text>
+          </Box>
         </Flex>
+        <Flex
+          flexDirection="column"
+          justifyContent="end"
+          alignItems="end"
+          flexGrow={1}
+          wrap="wrap"
+          py={200}
+          className="animate__animated animate__fadeIn animate__delay-3s"
+        >
+          <Box
+            display={"inline-block"}
+            alignSelf="center"
+            fontSize={{ sm: "2xl", md: "3xl", base: "2xl" }}
+            fontWeight="light"
+            px={200}
+          >
+            About
+          </Box>
+          <Box
+            display={"inline-block"}
+            alignSelf="center"
+            fontWeight={"lighter"}
+            flexWrap="wrap"
+            width={"55em"}
+            fontSize={{ sm: "sm", md: "md", base: "sm" }}
+            mt={4}
+          >
+            Hello! I'm Nijat Abdullazada, mainly backend, fullstack web
+            developer with a passion for building beautiful and functional web
+            applications. I have honed my skills in front-end development,
+            back-end development, and everything in between. I specialize in
+            working with modern web development frameworks and tools, including
+            React, Nest.js, Node.js, and more, and I am always eager to learn
+            and experiment with new technologies. If you want to work with me,
+            you can contact via email:
+          </Box>
+          <Box
+            as="a"
+            alignSelf="center"
+            justifySelf={"end"}
+            position={"absolute"}
+            bottom={"7rem"}
+            href="mailto:nicatabdullazade135@gmail.com"
+            {...borderBottomExpand}
+          >
+            Send Email
+          </Box>
+        </Flex>
+        <Socials />
       </Flex>
     </>
   );
