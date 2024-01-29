cfd4bb4d648334eeb401b7dd90904aae6aa3f4a1
HorrorOTB <nicatabdullazade135@gmail.com>
2024-01-29 22:05:06 +0400

    edit app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,20 @@
-import { useEffect, useState } from "react";
 import Admin from "./pages/Admin/Admin";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import "animate.css/animate.min.css";
-import StartScreen from "./components/StartScreen";
-import Frame from "./components/Frame";
 import BackgroundVideo from "./components/BackgroundVideo";
-import Projects from "./pages/Projects/Projects";
 
 function App() {
-  const [showStartScreen, setShowStartScreen] = useState(true);
-  const [showHome, setShowHome] = useState(false);
-
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setShowStartScreen(false);
-      setShowHome(true);
-    }, 2600);
-
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, []);
   return (
     <BrowserRouter>
-      <Frame>
-        <BackgroundVideo />
-
-        <div className="App">
-          <Routes>
-            {showHome && <Route path="/" element={<Home />}></Route>}
-            <Route path="/admin" element={<Admin />}></Route>
-            <Route path="/projects" element={<Projects />}></Route>
-          </Routes>
-        </div>
-      </Frame>
-
-      {showStartScreen && <StartScreen />}
+      <BackgroundVideo />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/admin" element={<Admin />}></Route>
+          {/* <Route path="/projects" element={<Projects />}></Route> */}
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
