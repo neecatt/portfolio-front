80ca28d0c5add89b6f1560efa5dc57862bd706e5
HorrorOTB <nicatabdullazade135@gmail.com>
2024-01-29 22:04:59 +0400

    edit frame

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,22 +1,16 @@
 import { Box } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 type TProps = {
   children: React.ReactNode;
 };
 
 const Frame = (props: TProps) => {
-  const [showFrame, setShowFrame] = useState(false);
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setShowFrame(true);
-    }, 2600);
-  }, []);
-  return showFrame ? (
+  return (
     <Box className="fixed top-0 left-0 right-0 bottom-0 overflow-hidden border m-4 md:m-3 animate__animated animate__fadeIn">
       {props.children}
     </Box>
-  ) : null;
+  );
 };
 
 export default Frame;
