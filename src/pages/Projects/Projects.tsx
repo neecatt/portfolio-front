c2f16df42e814e30529624911d7149e822c9e484
HorrorOTB <nicatabdullazade135@gmail.com>
2024-01-29 22:06:45 +0400

    edit projects layout

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,7 +1,110 @@
-import Project from "../../components/Project";
-
+import { ArrowUpIcon } from "@chakra-ui/icons";
+import {
+  Flex,
+  Box,
+  HStack,
+  SimpleGrid,
+  Stack,
+  VStack,
+  Button,
+} from "@chakra-ui/react";
+import { useEffect, useRef, useState } from "react";
+import ProjectCard from "../../components/ProjectCard";
+import ScrollToTop from "../../components/ScrollToTop";
 const Projects = () => {
-  return <Project />;
+  const [showScrollToTop, setShowScrollToTop] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const container = document.getElementById("projects");
+      if (!container) return;
+
+      const isAtBottom =
+        container.scrollHeight - container.scrollTop == container.clientHeight;
+
+      setShowScrollToTop(isAtBottom);
+    };
+
+    const container = document.getElementById("projects");
+    if (container) {
+      container.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (container) {
+        container.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
+
+  const handleScrollToTop = () => {
+    const container = document.getElementById("projects");
+    if (container) {
+      container.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    <Flex
+      flexDir={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      width={{ sm: "10%", base: "88%", md: "40%" }}
+      margin={"8em auto"}
+      height={"36em"}
+    >
+      <Flex
+        id="projects"
+        justifyContent={"center"}
+        alignItems={"center"}
+        flexWrap={"wrap"}
+        overflow={"auto"}
+        gap={5}
+        p={"1em"}
+        sx={{
+          "&::-webkit-scrollbar": {
+            display: "none",
+          },
+        }}
+      >
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </Flex>
+      <Flex mb={"1rem"} justifyContent={"center"} w={"100%"}>
+        {showScrollToTop && (
+          <Box
+            as="button"
+            onClick={handleScrollToTop}
+            className="animate__animated animate__fadeIn"
+            _hover={{
+              cursor: "pointer",
+              backgroundColor: "gray.700",
+              borderRadius: "70%",
+            }}
+            boxSize={"2rem"}
+          >
+            <ScrollToTop />
+          </Box>
+        )}
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Projects;
