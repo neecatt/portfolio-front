2a0685544c1bf641c5a180787d57c327ee91ec7c
HorrorOTB <nicatabdullazade135@gmail.com>
2024-01-29 22:06:35 +0400

    edit tab change

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,12 @@
-import { Box, Flex, Img, Text, Wrap } from "@chakra-ui/react";
+import { Box, Flex, Text, Tabs, TabPanels, TabPanel } from "@chakra-ui/react";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar";
 import Socials from "../../components/Socials";
+import StartScreen from "../../components/StartScreen";
 import borderBottomExpand from "../../props/borderBottomExpand";
-
+import Frame from "../../components/Frame";
+import Projects from "../Projects/Projects";
 const Home = () => {
   const typingContainer = {
     hidden: { opacity: 0 },
@@ -26,108 +29,111 @@ const Home = () => {
     },
   };
 
+  const handleTabChange = (index: number) => {
+    setShowStartScreen(false);
+    setActiveTab(index);
+    setPageTransitionOut(true); // Apply fade-out animation class
+    setTimeout(() => {
+      setActiveTab(index);
+      setPageTransitionIn(true); // Apply fade-in animation class
+    }, 500);
+  };
+
+  const [showStartScreen, setShowStartScreen] = useState(true);
+  const [showHome, setShowHome] = useState(false);
+  const [activeTab, setActiveTab] = useState<number>(0);
+  const [pageTransitionOut, setPageTransitionOut] = useState<boolean>(false);
+  const [pageTransitionIn, setPageTransitionIn] = useState<boolean>(true);
+
+  useEffect(() => {
+    console.log(activeTab);
+    const timeoutId = setTimeout(() => {
+      setShowStartScreen(!showStartScreen);
+      setShowHome(!showHome);
+    }, 2600);
+    setPageTransitionIn(false);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [activeTab]);
+
   return (
     <>
-      <Navbar />
-      <Flex
-        height="100vh"
-        overflow="hidden"
-        overscrollBehavior={"none"}
-        className="animate__animated animate__fadeIn"
-        zIndex={-999}
-        px={4}
-      >
-        <Flex
-          justifyContent={"start"}
-          flexDirection="column"
-          py={"10rem"}
-          // border="1px solid blue"
-        >
-          <Box
-            fontSize={{ sm: "5xl", md: "7xl", base: "3xl" }}
-            display={"inline-block"}
-            fontWeight="light"
+      {showStartScreen ? (
+        <StartScreen />
+      ) : (
+        <Frame>
+          <motion.div
+            key={activeTab}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+            className={`${pageTransitionOut ? "animate__fadeOut" : ""} ${
+              pageTransitionIn ? "animate__fadeIn" : ""
+            }`}
           >
-            <motion.div
-              variants={typingContainer}
-              initial="hidden"
-              animate="show"
-            >
-              {Array.from("Nijat Abdullazada").map((word, i) => (
-                <motion.span key={i} variants={typingText}>
-                  {word}
-                </motion.span>
-              ))}
-            </motion.div>
-          </Box>
-          <Box>
-            <Text
-              className="animate__animated animate__fadeIn animate__delay-1s"
-              fontSize={"3xl"}
-              display={"inline-block"}
-              fontWeight="lighter"
-              sx={{
-                "--animate-delay": "1.4s",
-              }}
-            >
-              Web Developer
-            </Text>
-          </Box>
-        </Flex>
-        <Flex
-          flexDirection="column"
-          justifyContent="end"
-          alignItems="end"
-          flexGrow={1}
-          wrap="wrap"
-          py={200}
-          className="animate__animated animate__fadeIn animate__delay-3s"
-          sx={{
-            "--animate-delay": "0.5s",
-          }}
-        >
-          <Box
-            display={"inline-block"}
-            alignSelf="center"
-            fontSize={{ sm: "2xl", md: "3xl", base: "2xl" }}
-            fontWeight="light"
-            px={200}
-          >
-            About
-          </Box>
-          <Box
-            display={"inline-block"}
-            alignSelf="center"
-            fontWeight={"lighter"}
-            flexWrap="wrap"
-            width={"55em"}
-            fontSize={{ sm: "sm", md: "md", base: "sm" }}
-            mt={4}
-          >
-            Hello! I'm Nijat Abdullazada, mainly backend, fullstack web
-            developer with a passion for building beautiful and functional web
-            applications. I have honed my skills in front-end development,
-            back-end development, and everything in between. I specialize in
-            working with modern web development frameworks and tools, including
-            React, Nest.js, Node.js, and more, and I am always eager to learn
-            and experiment with new technologies. If you want to work with me,
-            you can contact via email:
-          </Box>
-          <Box
-            as="a"
-            alignSelf="center"
-            justifySelf={"end"}
-            position={"absolute"}
-            bottom={"7rem"}
-            href="mailto:nicatabdullazade135@gmail.com"
-            target={"_blank"}
-            {...borderBottomExpand}
-          >
-            Send Email
-          </Box>
-        </Flex>
-        <Socials />
-      </Flex>
+            <Tabs variant={"unstyled"} colorScheme={"none"}>
+              <Navbar />
+              <TabPanels>
+                <TabPanel>
+                  <Flex
+                    height="100vh"
+                    overflow="hidden"
+                    overscrollBehavior={"none"}
+                    className="animate__animated animate__fadeIn"
+                    zIndex={-999}
+                    px={4}
+                  >
+                    <Flex
+                      justifyContent={"start"}
+                      flexDirection="column"
+                      py={"10rem"}
+                      // border="1px solid blue"
+                    >
+                      <Box
+                        fontSize={{ sm: "5xl", md: "7xl", base: "3xl" }}
+                        display={"inline-block"}
+                        fontWeight="light"
+                      >
+                        <motion.div
+                          variants={typingContainer}
+                          initial="hidden"
+                          animate="show"
+                        >
+                          {Array.from("Nijat Abdullazada").map((word, i) => (
+                            <motion.span key={i} variants={typingText}>
+                              {word}
+                            </motion.span>
+                          ))}
+                        </motion.div>
+                      </Box>
+                      <Box>
+                        <Text
+                          className="animate__animated animate__fadeIn animate__delay-1s"
+                          fontSize={"3xl"}
+                          display={"inline-block"}
+                          fontWeight="lighter"
+                          sx={{
+                            "--animate-delay": "1.2s",
+                          }}
+                        >
+                          Software Developer
+                        </Text>
+                      </Box>
+                    </Flex>
+                    <Socials />
+                  </Flex>
+                </TabPanel>
+                <TabPanel>
+                  <Projects />
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </motion.div>
+        </Frame>
+      )}
     </>
   );
 };
