f35f3680a06a1047d57eb0dcc49b44711da90d6d
HorrorOTB <nicatabdullazade135@gmail.com>
2024-01-29 22:05:23 +0400

    add scroll icon

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,8 @@
+import { ArrowUpIcon } from "@chakra-ui/icons";
+import React from "react";
+
+const ScrollToTop = () => {
+  return <ArrowUpIcon className="animate">ScrollToTop</ArrowUpIcon>;
+};
+
+export default ScrollToTop;
