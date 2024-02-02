f549913851f5534624b5c203db621ce6f3d12d7a
HorrorOTB <nicatabdullazade135@gmail.com>
2024-02-02 18:50:51 +0400

    modify structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
-import Admin from "./pages/Admin/Admin";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
 import "animate.css/animate.min.css";
 import BackgroundVideo from "./components/BackgroundVideo";
+import Frame from "./components/Frame";
+import Navbar from "./components/Navbar";
+import { useEffect, useState } from "react";
+import StartScreen from "./components/StartScreen";
+import AppRoutes from "./components/Routes";
+import { Flex } from "@chakra-ui/react";
 
 function App() {
+  const [showStartScreen, setShowStartScreen] = useState(true);
+  const [showHome, setShowHome] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowStartScreen(!showStartScreen);
+      setShowHome(!showHome);
+    }, 2500);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
   return (
-    <BrowserRouter>
-      <BackgroundVideo />
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/admin" element={<Admin />}></Route>
-          {/* <Route path="/projects" element={<Projects />}></Route> */}
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Flex>
+      {showStartScreen ? (
+        <StartScreen />
+      ) : (
+        <Frame>
+          <BackgroundVideo />
+
+          <div className="h-[3.8rem]">
+            <Navbar />
+          </div>
+          <div className="h-auto">
+            <AppRoutes />
+          </div>
+        </Frame>
+      )}
+    </Flex>
   );
 }
 
