49e557317804683a217c2b6314062cff40e1eb6a
HorrorOTB <nicatabdullazade135@gmail.com>
2024-07-08 00:58:39 +0400

    feat: Fetch experiences and projects from API and update both pages

--- src/pages/Experience/Experience.tsx
+++ src/pages/Experience/Experience.tsx
@@ -1,54 +1,67 @@
 import { Box, Flex } from "@chakra-ui/react";
 import { TimelineStep } from "../../components";
 import { TJob } from "../../types/job.type";
+import { useEffect, useState } from "react";
+import getExperiences from "../../api/getExperiences";
 import "./Experience.css";
 
 const Experience = () => {
-  const jobs = [
-    {
-      jobTitle: "Software Developer",
-      companyName: "Morpho",
-      date: "Jun 2023 - Present",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      latest: true,
-    },
-    {
-      jobTitle: "Software Developer",
-      companyName: "Morpho",
-      date: "Jun 2023 - Present",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      latest: true,
-    },
-    {
-      jobTitle: "Software Developer",
-      companyName: "Morpho",
-      date: "Jun 2023 - Present",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      latest: true,
-    },
-    {
-      jobTitle: "Software Developer",
-      companyName: "Morpho",
-      date: "Jun 2023 - Present",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      latest: true,
-    },
-    {
-      jobTitle: "Software Developer",
-      companyName: "Morpho",
-      date: "Jun 2023 - Present",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      latest: true,
-    },
-    {
-      jobTitle: "Software Developer",
-      companyName: "Morpho",
-      date: "Jun 2023 - Present",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      latest: true,
-    },
-  ];
+  const [jobs, setJobs] = useState<TJob[]>([]);
+
+  useEffect(() => {
+    const fetchExperiences = async () => {
+      const jobs = await getExperiences();
+      setJobs(jobs);
+    };
+
+    fetchExperiences();
+  }, []);
+
+  // const jobs = [
+  //   {
+  //     jobTitle: "Software Developer",
+  //     companyName: "Morpho",
+  //     date: "Jun 2023 - Present",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //     latest: true,
+  //   },
+  //   {
+  //     jobTitle: "Software Developer",
+  //     companyName: "Morpho",
+  //     date: "Jun 2023 - Present",
+  //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //     latest: true,
+  //   },
+  //   {
+  //     jobTitle: "Software Developer",
+  //     companyName: "Morpho",
+  //     date: "Jun 2023 - Present",
+  //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //     latest: true,
+  //   },
+  //   {
+  //     jobTitle: "Software Developer",
+  //     companyName: "Morpho",
+  //     date: "Jun 2023 - Present",
+  //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //     latest: true,
+  //   },
+  //   {
+  //     jobTitle: "Software Developer",
+  //     companyName: "Morpho",
+  //     date: "Jun 2023 - Present",
+  //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //     latest: true,
+  //   },
+  //   {
+  //     jobTitle: "Software Developer",
+  //     companyName: "Morpho",
+  //     date: "Jun 2023 - Present",
+  //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  //     latest: true,
+  //   },
+  // ];
 
   return (
     <div className="flex justify-center items-center w-auto h-[80vh] overflow-y-auto flex-wrap mt-[5em] p-[1em]">
